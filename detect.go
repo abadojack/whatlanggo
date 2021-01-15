1248096993fab12b28dfe0c3cbd62a0f7753ae5d
Matthias Schneider <ms@wck.biz>
2021-01-15 10:27:00 +0100

    fixes #20 by improving calculateDistance

--- detect.go
+++ detect.go
@@ -1,6 +1,7 @@
 package whatlanggo
 
 import (
+	"math"
 	"sort"
 	"unicode"
 )
@@ -93,13 +94,13 @@ func detectLangBaseOnScript(text string, options Options, script *unicode.RangeT
 }
 
 type langDistance struct {
-	lang Lang
-	dist int
+	lang        Lang
+	dist        int
+	numTrigrams int
 }
 
 func detectLangInProfiles(text string, options Options, langProfileList langProfileList) (Lang, float64) {
-	trigrams := getTrigramsWithPositions(text)
-
+	trigramWeights := count(text)
 	langDistances := []langDistance{}
 
 	for lang, langTrigrams := range langProfileList {
@@ -114,9 +115,8 @@ func detectLangInProfiles(text string, options Options, langProfileList langProf
 				continue
 			}
 		}
-
-		dist := calculateDistance(langTrigrams, trigrams)
-		langDistances = append(langDistances, langDistance{lang, dist})
+		dist, numberTrigrams := calculateDistance(langTrigrams, trigramWeights)
+		langDistances = append(langDistances, langDistance{lang, dist, numberTrigrams})
 	}
 
 	switch len(langDistances) {
@@ -125,18 +125,20 @@ func detectLangInProfiles(text string, options Options, langProfileList langProf
 	case 1:
 		return langDistances[0].lang, 1
 	default:
-		return calculateConfidence(langDistances, trigrams)
+		return calculateConfidence(langDistances)
 	}
 }
 
-func calculateConfidence(langDistances []langDistance, trigrams map[string]int) (Lang, float64) {
-	sort.SliceStable(langDistances, func(i, j int) bool { return langDistances[i].dist < langDistances[j].dist })
+func calculateConfidence(langDistances []langDistance) (Lang, float64) {
+	sort.SliceStable(langDistances, func(i, j int) bool { return langDistances[i].dist > langDistances[j].dist })
+
 	langDist1 := langDistances[0]
 	langDist2 := langDistances[1]
-	score1 := maxTotalDistance - langDist1.dist
-	score2 := maxTotalDistance - langDist2.dist
+	score1 := langDist1.dist
+	score2 := langDist2.dist
 
 	var confidence float64
+
 	if score1 == 0 {
 		// If score1 is 0, score2 is 0 as well, because array is sorted.
 		// Therefore there is no language to return.
@@ -157,31 +159,26 @@ func calculateConfidence(langDistances []langDistance, trigrams map[string]int) 
 	}
 
 	rate := float64((score1 - score2)) / float64(score2)
-
 	// Hyperbola function. Everything that is above the function has confidence = 1.0
 	// If rate is below, confidence is calculated proportionally.
 	// Numbers 12.0 and 0.05 are obtained experimentally, so the function represents common sense.
 
-	confidentRate := float64(12.0/float64(len(trigrams))) + 0.05
+	confidentRate := float64(12.0/float64(langDist1.numTrigrams)) + 0.05
 	if rate > confidentRate {
 		confidence = 1.0
 	} else {
 		confidence = rate / confidentRate
 	}
-
+	confidence = math.Round(confidence*100) / 100
 	return langDist1.lang, confidence
 }
 
-func calculateDistance(langTrigrams []string, textTrigrams map[string]int) int {
-	var dist, totalDist int
+func calculateDistance(langTrigrams []string, textTrigramsWeights map[string]int) (totalDist, numberTrigrams int) {
 	for i, trigram := range langTrigrams {
-		if n, ok := textTrigrams[trigram]; ok {
-			dist = abs(n - i)
-		} else {
-			dist = maxTrigramDistance
+		if textTrigramsWeights[trigram] > 0 {
+			numberTrigrams++
+			totalDist += (maxTrigramDistance - i) * textTrigramsWeights[trigram]
 		}
-		totalDist += dist
 	}
-
-	return totalDist
+	return totalDist, numberTrigrams
 }
